74305bdae6f5dc5c12587c30a70abefadf0a874a
Ben Jeffery <ben.jeffery@gmail.com>
2009-11-25 03:24:21 +0000

    Example bot builds new probes and pylons

--- src/agents/econ_agent.py
+++ src/agents/econ_agent.py
@@ -1,6 +1,7 @@
 import time
 import math
 from operator import itemgetter
+import random
 
 def dist(unit1, unit2):
     dx = float(unit1.x)-float(unit2.x)
@@ -17,7 +18,23 @@ def loop(game, command):
             #Shitty algo but one line!
             best_mineral, distance = sorted([(mineral, dist(mineral, probe)) for mineral in minerals],key=itemgetter(1))[0]
             command.right_click_unit(probe, best_mineral)
-        time.sleep(.1)
+        if game.me['minerals'] >= 50:
+            try:
+                nexus = (unit for unit in game.my_units if unit.type == 'protoss_nexus' and unit.train_timer == "0").next()
+                command.train(nexus, 'protoss_probe')
+            except StopIteration:
+                pass
+        if game.me['minerals'] > 100 and game.me['supply']-game.me['supply_used'] <= 2:
+            try:
+                probe = (unit for unit in game.my_units if unit.type == 'protoss_probe').next()
+                command.build(probe, 
+                              int(probe.x) + random.uniform(-10,10),
+                              int(probe.y) + random.uniform(-10,10),
+                              "protoss_pylon")
+            except StopIteration:
+                pass
+                
+        time.sleep(.5)
     print "Agent ending"
 
 print __file__,"loaded"
